08a6f74ff79f5116744a71f9996ec4cc3ba58a69
Mara Schulke <mara.schulke@helsing.ai>
2025-03-08 21:47:47 +0100

    Make the extension compile!

--- flake.nix
+++ flake.nix
@@ -24,8 +24,45 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        supportedPostgres = with pkgs; [
+          postgresql_13
+          postgresql_14
+          postgresql_15
+          postgresql_16
+          postgresql_17
+        ];
+
+        listToAttrset = list: fn: lib.foldl' lib.attrsets.recursiveUpdate { } (map fn list);
+
+        packages = listToAttrset supportedPostgres (
+          postgresql:
+          let
+            major = lib.versions.major postgresql.version;
+          in
+          {
+            "pgpt-${major}" = polar.lib.buildPgrxExtension {
+              inherit system postgresql;
+              src = ./.;
+            };
+          }
+        );
+
+        checks = listToAttrset supportedPostgres (
+          postgresql:
+          let
+            major = lib.versions.major postgresql.version;
+          in
+          {
+            "pgpt-${major}" = self.packages.${system}."pgpt-${major}";
+          }
+        );
       in
       {
+        inherit packages checks;
+
         devShells.default = pkgs.mkShell {
           buildInputs =
             with pkgs;
@@ -48,20 +85,19 @@
             ];
 
           shellHook = ''
+            export RUSTFLAGS="-Clink-args=-Wl,-undefined,dynamic_lookup";
             export PKG_CONFIG_PATH="${pkgs.icu}/lib/pkgconfig";
-            export LDFLAGS="-L${pkgs.icu}/lib"
-            export CPPFLAGS="-I${pkgs.icu}/include"
+            export LDFLAGS="-L${pkgs.icu}/lib";
+            export CPPFLAGS="-I${pkgs.icu}/include";
+
+            # by default, run against pg17
+            PG_VERSION=pg17
+
+            pgrx-install() {
+              cargo pgrx install -c $HOME/.pgrx/17.*/pgrx-install/bin/pg_config
+            }
           '';
         };
-
-        packages.pgpt = polar.lib.buildPgrxExtension {
-          inherit system;
-
-          postgresql = pkgs.postgresql_16;
-          src = ./.;
-        };
-
-        checks.pgpt = self.packages.${system}.pgpt;
       }
     );
 }
